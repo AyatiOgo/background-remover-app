1c9638e900f2fc3c86b71f61bee176d71fc67d42
Ayati <ayatiogo@gmail.com>
2025-08-11 00:25:37 +0100

    bg remover functionality added working version_1

--- removerApp/views.py
+++ removerApp/views.py
@@ -1,13 +1,31 @@
+from django.core.files.base import ContentFile
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Image
 from .forms import UploadForm
+from rembg import remove
 # Create your views here.
 
 def upload_image(request):
     if request.method == 'POST':
         form = UploadForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            original_img = form.cleaned_data["original_img"]
+            imagebyte = original_img.read()
+            edited  = remove(imagebyte, force_return_bytes=True)
+            response = HttpResponse(edited, content_type="image/png")
+            response['Content-Disposition'] = 'attachment; filename="no-bg.png"'
+            
+            return response
+
+
+
+            # output = ContentFile(edited,  name='no-bg.png')
+            # obj = Image()
+            # obj.original_img= original_img
+            # obj.new_img.save('no-bg.png', output)
+            # obj.save()
+            
     else:
         form = UploadForm()
 
