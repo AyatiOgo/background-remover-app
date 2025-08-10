878799d4f34022b1d5560a5e7d658c59adf5bb51
Ayati <ayatiogo@gmail.com>
2025-08-10 22:08:12 +0100

    upload form and models added

--- removerApp/views.py
+++ removerApp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import Image
+from .forms import UploadForm
 # Create your views here.
+
+def upload_image(request):
+    if request.method == 'POST':
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UploadForm()
+
+    return render(request, "index.html", {'form':form})
