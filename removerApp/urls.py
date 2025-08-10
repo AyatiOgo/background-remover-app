63d96ddb848ec9f603d608ec8561d12b0b8129f7
Ayati <ayatiogo@gmail.com>
2025-08-10 22:15:16 +0100

    media URL and directory configured

--- removerApp/urls.py
+++ removerApp/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import upload_image
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', upload_image, name='home'),
+    
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
