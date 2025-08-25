d1d1bbe10bf07b5776555dd516b4d07bc6875779
Ayati <ayatiogo@gmail.com>
2025-08-25 22:08:05 +0100

    ready for deployment

--- bg_remove/settings.py
+++ bg_remove/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3zs7fe-f1_-j$_&x(*n^pj+0gyt6@a)5slru=40uo)dnu#n@ro'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
